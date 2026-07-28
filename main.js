167fbb45156bafee4e05ca9d3b7762d3a78a4ab3
copias-salva <copias@salvador.org.br>
2026-07-28 15:42:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-function createWindow() {
+function createWindow () {
   const win = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    title: "Gestão de Equipes",
-    autoHideMenuBar: true,
+    width: 1024,
+    height: 768,
+    icon: path.join(__dirname, 'icon.ico'),
     webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true
+      nodeIntegration: true,
+      contextIsolation: false
     }
   });
 
   win.loadFile('index.html');
 }
 
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
